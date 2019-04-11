a995e5a59681264c1843ce731dc54c66a0e58a77
Thiago Bonfim <thiakau@gmail.com>
2019-04-11 15:24:47 -0700

    Adding Redux to traffic light

--- src/component/trafficLight.js
+++ src/component/trafficLight.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
-
+import {connect} from "react-redux";
 import SingleLight from "./singleLight";
 
-export default class TrafficLight extends Component {
+class TrafficLight extends Component {
   constructor() {
     super();
     this.lights = ["red", "yellow", "green"];
-    this.state = {
-      active: -1
-    };
 
     this.handleLightCLick = this.handleLightCLick.bind(this);
   }
 
   handleLightCLick(e) {
     const active = this.lights.indexOf(e.currentTarget.dataset.color);
-    this.setState({
-      active: active
-    });
+    this.props.dispatch({ type: 'ACTIVATE', active: active} );
+    // this.setState({
+    //   active: active
+    // });
   }
 
   render() {
@@ -29,7 +27,7 @@ export default class TrafficLight extends Component {
               key={light}
               color={light}
               onClick={this.handleLightCLick}
-              active={this.state.active === index}
+              active={this.props.active === index}
             />
           );
         })}
@@ -37,3 +35,7 @@ export default class TrafficLight extends Component {
     );
   }
 }
+const mapStateToProps = (state) => ({
+  active: state.active
+});
+export default connect(mapStateToProps)(TrafficLight);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
 import TrafficLight from "./component/trafficLight";
 
 import "./styles.css";
+const initialState = { active:-1 };
 
+function reducer( state=initialState, action){
+  switch (action.type) {
+    case "ACTIVATE":
+      return { active: action.active};
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store} className="App">
         <TrafficLight />
-      </div>
+      </Provider>
     );
   }
 }
